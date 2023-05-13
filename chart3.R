7669eaa39840dd479449839247362c3a361be7a6
gcasta11 <129224032+gcasta11@users.noreply.github.com>
2023-05-12 22:49:50 -0700

    Update chart3.R

--- chart3.R
+++ chart3.R
@@ -5,7 +5,7 @@ chart3 <- ggplot(data = spl_df) +
     y = Checkouts, 
     color = UsageClass)) +
   facet_wrap(~UsageClass) +
-  labs(title = "Physical Book Checkouts 2017-2023", 
+  labs(title = "Difference in Book Checkouts 2017-2023", 
        x = "Year", 
        y = "Number of Checkouts")
 chart3
