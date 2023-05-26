e1f16144d403685814655d535dc5ccda96cc670e
gcasta11 <129224032+gcasta11@users.noreply.github.com>
2023-05-26 01:38:04 -0700

    Update chart2.R

--- chart2.R
+++ chart2.R
@@ -1,4 +1,5 @@
 # plotting chart 2
+spl_df <- read.csv("~/Downloads/info_201_code/2017-2023-10-Checkouts-SPL-Data.csv")
 num_checkouts_digital <- c(num_digital_checkouts_2018,
                             num_digital_checkouts_2019, 
                             num_digital_checkouts_2020, 
@@ -6,15 +7,15 @@ num_checkouts_digital <- c(num_digital_checkouts_2018,
                             num_digital_checkouts_2022)
 time <- c(2018, 2019, 2020, 2021, 2022)
 
-
+library(dplyr)
 library(ggplot2)
 options(scipen = 999)
-ggplot() +
+chart2 <- ggplot() +
   geom_line(aes(x= time, y = num_checkouts_digital)) +
   labs(title = "Digital Book Checkouts 2018-2022", 
        x = "Year", 
-       y = "Number of Digital Checkouts") 
-scale_x_continuous(limits = c(2018, 2022))
+       y = "Number of Digital Checkouts") + 
+  scale_x_continuous(limits = c(2018, 2022))
 
 
-
+chart2
