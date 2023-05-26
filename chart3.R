825aeab48f2163a4466bf60092a66503337b635e
gcasta11 <129224032+gcasta11@users.noreply.github.com>
2023-05-26 01:38:28 -0700

    Update chart3.R

--- chart3.R
+++ chart3.R
@@ -1,11 +1,13 @@
+library(dplyr)
+spl_df <- read.csv("~/Downloads/info_201_code/2017-2023-10-Checkouts-SPL-Data.csv")
 #plotting chart 3
-ggplot(data = spl_df) +
+chart3 <- ggplot(data = spl_df) +
   geom_col(mapping = aes(
     x = CheckoutYear, 
     y = Checkouts, 
-    color = UsageClass)) +
+    fill = UsageClass)) +
   facet_wrap(~UsageClass) +
   labs(title = "Difference in Book Checkouts 2017-2023", 
        x = "Year", 
        y = "Number of Checkouts")
-
+chart3
